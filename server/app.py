a3a991b8bebab438a62fd13637dd6711b041db88
Kat Tannehill <katetannehill@gmail.com>
2023-11-20 20:52:31 -0700

    Complete lab

--- server/app.py
+++ server/app.py
@@ -52,6 +52,29 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter_by(username=request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+        if user := User.query.filter_by(id=session.get('user_id')).first():
+            return user.to_dict(), 200
+        return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
